16363e36736fed4c059fb345ac081f9fec968501
Daniel Ireson <daniel@djgraphics.co.uk>
2016-09-14 01:10:36 +0100

    Cleaned up footer to look nicer on mobile

--- src/components/FooterComponent.vue
+++ src/components/FooterComponent.vue
@@ -2,8 +2,8 @@
   <footer class="footer">
     <div class="container">
       <div class="content has-text-centered">
-        <p><strong>Megabus Search</strong> by <a href="http://digitalbydan.com">Daniel Ireson</a>. Use at your own discretion.</p>
-        <p>Not associated with Megabus.com.</p>
+        <p><strong>Megabus Search</strong> by <a href="http://digitalbydan.com">Daniel Ireson</a></p>
+        <p>Not associated with megabus.com</p>
       </div>
     </div>
   </footer>
