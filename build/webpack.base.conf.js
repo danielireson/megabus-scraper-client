6fe1873e63a6b2076d488a1ef481b6ec7c3bc491
Daniel Ireson <daniel@djgraphics.co.uk>
2017-01-28 12:01:01 +0000

    use env variables via dotenv

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -2,6 +2,7 @@ var path = require('path')
 var config = require('../config')
 var utils = require('./utils')
 var projectRoot = path.resolve(__dirname, '../')
+var Dotenv = require('dotenv-webpack')
 
 module.exports = {
   entry: {
@@ -81,5 +82,8 @@ module.exports = {
   },
   vue: {
     loaders: utils.cssLoaders()
-  }
+  },
+  plugins: [
+    new Dotenv()
+  ]
 }
