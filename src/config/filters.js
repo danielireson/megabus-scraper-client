1e7543152082e87a4b0b5eeb61d17b581b7c63ff
Daniel Ireson <daniel@djgraphics.co.uk>
2017-01-30 15:04:23 +0000

    remove unused filters file

--- src/config/filters.js
+++
@@ -1,7 +0,0 @@
-import Vue from 'vue'
-
-Vue.filter('titleCase', function (value) {
-  return value.replace(/\w\S*/g, function (txt) {
-    return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase()
-  })
-})
--- src/main.js
+++ src/main.js
@@ -1,7 +1,6 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 
-import './config/filters'
 import './config/http'
 
 import Home from './pages/Home'
