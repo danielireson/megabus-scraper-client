84f500f960102782f0387b4d1a6cf3afe9f4dfaa
Daniel Ireson <daniel@djgraphics.co.uk>
2017-03-10 19:18:40 +0000

    update notificationService to allow for permanent notifications

--- src/services/NotificationService.js
+++ src/services/NotificationService.js
@@ -5,14 +5,16 @@ var NotificationService = {
     type: '',
     timer: null
   },
-  showMessage (text, type = 'info') {
+  showMessage (text, type = 'info', timeout = true) {
     this.state.text = text
     this.state.type = type
     this.state.visible = true
     clearTimeout(this.state.timer)
-    this.state.timer = setTimeout(() => {
-      this.state.visible = false
-    }, 5000)
+    if (timeout) {
+      this.state.timer = setTimeout(() => {
+        this.state.visible = false
+      }, 5000)
+    }
   },
   hideMessage () {
     this.state.visible = false
